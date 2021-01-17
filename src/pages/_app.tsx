26fad8f14e9d750ddf68cd0ce5623b4d91aac055
Denmark-banana <neat507kr@gmail.com>
2021-01-17 19:38:36 +0900

    feat(components/atoms): add label component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import Head from 'next/head';
 import GlobalStyles from 'styles/GlobalStyles';
+import { AppProps } from 'next/app';
 
 import { ThemeProvider } from 'styled-components';
 import Themes from 'styles/Theme';
+import { Label } from 'components/atoms/Label';
 
-const MyApp = () => {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ThemeProvider theme={Themes}>
       <Head>
         <title>타이틀</title>
       </Head>
       <GlobalStyles />
+      <Component {...pageProps} />
     </ThemeProvider>
   );
 };
