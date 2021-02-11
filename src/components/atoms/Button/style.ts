3e838f5acda9279d1967679215eadb76a04c93b9
spring <dongwon1826@gmail.com>
2021-02-11 14:42:12 +0900

    refactor(components/atom) : fontSize에fontType 적용

--- src/components/atoms/Button/style.ts
+++ src/components/atoms/Button/style.ts
@@ -1,40 +1,39 @@
-import styled, { css } from "styled-components";
-import { ButtonProps } from "./index";
+import styled, { css } from 'styled-components';
+import { ButtonProps } from './index';
 
 export const Wrap = styled.button<ButtonProps>`
-  width: ${(props) => props.width && props.width};
-  margin: ${(props) => props.margin && props.margin};
-  margin: ${(props) => props.margin && props.margin};
-  padding: ${(props) => props.padding && props.padding};
-  color: ${(props) => props.color && props.color};
+  width: ${props => props.width && props.width};
+  margin: ${props => props.margin && props.margin};
+  padding: ${props => props.padding && props.padding};
+  color: ${props => props.color && props.color};
   border-radius: 5px;
-  font: ${(props) => props.fontSize && props.theme.fonts[props.fontSize]};
+  font: ${props => props.fontSize && props.theme.fonts[props.fontSize]};
   cursor: pointer;
-  ${(props) => {
+  ${props => {
     if (props.filled) {
       return css`
-        color: ${(props) => props.theme.colors.WHITE};
-        background-color: ${(props) => props.theme.colors.MAIN};
-        border: ${(props) => props.theme.colors.WHITE};
+        color: ${props => props.theme.colors.WHITE};
+        background-color: ${props => props.theme.colors.MAIN};
+        border: ${props => props.theme.colors.WHITE};
       `;
     }
     if (!props.filled) {
       return css`
-        color: ${(props) => props.theme.colors.MAIN};
-        background-color: ${(props) => props.theme.colors.WHITE};
-        border: ${(props) => props.theme.colors.MAIN};
+        color: ${props => props.theme.colors.MAIN};
+        background-color: ${props => props.theme.colors.WHITE};
+        border: ${props => props.theme.colors.MAIN};
       `;
     }
     if (props.disabled) {
       return css`
-        color: ${(props) => props.theme.colors.BLACK};
-        background-color: ${(props) => props.theme.colors.GRAY[400]};
-        border: ${(props) => props.theme.colors.GRAY[400]};
+        color: ${props => props.theme.colors.BLACK};
+        background-color: ${props => props.theme.colors.GRAY[400]};
+        border: ${props => props.theme.colors.GRAY[400]};
       `;
     }
   }}
   &:hover {
-    background-color: ${(props) => props.theme.colors.DARK};
+    background-color: ${props => props.theme.colors.DARK};
   }
   &:focus {
     outline: none;
