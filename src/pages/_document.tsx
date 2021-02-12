700653aa85105041ed6c88eea6dd8487c94c6a52
Denmark-banana <neat507kr@gmail.com>
2021-02-12 12:39:20 +0900

    chore : styledCoponent에 ssr 세팅 적용

---
+++ src/pages/_document.tsx
@@ -0,0 +1,51 @@
+// import * as React from 'react';
+import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+interface StyledProps {
+  styleTags: Array<React.ReactElement<{}>>;
+}
+
+class MyDocument extends Document<StyledProps> {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+    const initialProps = await Document.getInitialProps(ctx);
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+        });
+
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } catch (error) {
+      console.error(error);
+    } finally {
+      sheet.seal();
+    }
+    console.error('return Origin initialProps');
+    return initialProps;
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
