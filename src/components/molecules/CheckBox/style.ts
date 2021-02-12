8194fe4184def3e87eaad78d2ca21ab9e8896185
spring1826 <dongwon1826@gmail.com>
2021-02-12 13:56:26 +0900

    feat(components/atoms) : checkbox 컴포넌트 구현 (#13)
    * feat(components/atoms) : checkbox 컴포넌트 구현
    
    * refactor(components/molecules) : checkbox 위치 수정

---
+++ src/components/molecules/CheckBox/style.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import { CheckBoxProps } from './index';
+
+export const Wrap = styled.div``;
+
+export const CheckBoxLabel = styled.label<CheckBoxProps>`
+  display: flex;
+`;
+export const CheckBoxInput = styled.input`
+  display: none;
+`;
+export const IconContainer = styled.div<CheckBoxProps>`
+  color: ${props => props.theme.colors.MAIN};
+  font-size: ${props => props.iconSize && props.iconSize};
+`;
