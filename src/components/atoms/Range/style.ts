23c4f5fc0f138a963f221f916b7ce540df736f64
spring1826 <dongwon1826@gmail.com>
2021-02-12 19:31:08 +0900

    refactor(components/atoms) : range에 mixin적용 (#30)

--- src/components/atoms/Range/style.ts
+++ src/components/atoms/Range/style.ts
@@ -1,7 +1,10 @@
+import { ComponentMixin } from 'common';
+import { IRangeProps } from './index';
 import styled from 'styled-components';
 
-export const Wrap = styled.input`
-  width: ${props => props.width && props.width};
+export const Wrap = styled.input<IRangeProps>`
+  ${ComponentMixin};
+  cursor: pointer;
   &::-webkit-slider-thumb {
     -webkit-appearance: none;
     margin-top: -4px;
@@ -9,14 +12,10 @@ export const Wrap = styled.input`
     height: 12px;
     border-radius: 6px;
     background-color: ${props => props.theme.colors.MAIN};
-    cursor: pointer;
   }
   &::-webkit-slider-runnable-track {
     height: 4px;
     background-color: ${props => props.theme.colors.GRAY[400]};
     border-radius: 10px;
-  }
-  &:focus::-ms-fill-lower {
-    background-color: ${props => props.theme.colors.MAIN};
   }
 `;
