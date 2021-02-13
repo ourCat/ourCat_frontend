f591af4ce8c31db054ebdda77cf3e3ce2b87edda
spring1826 <dongwon1826@gmail.com>
2021-02-13 16:40:22 +0900

    Feat/add recommendation component (#35)
    * refactor(components/atoms) : image tag 수정
    
    * feat(components/molcules) : Recommend컴포넌트 구현
    
    * refactor(components/molcules) : label => labelText

--- src/components/atoms/Image/style.ts
+++ src/components/atoms/Image/style.ts
@@ -2,9 +2,9 @@ import styled from 'styled-components';
 import { ComponentMixin } from 'common';
 import { ImageProps } from './index';
 
-export const WrapImage = styled.image<ImageProps>`
+export const WrapImage = styled.img<ImageProps>`
   ${ComponentMixin};
-  src: ${props => props.url && props.url};
+  src: ${props => props.src && props.src};
   cursor: ${props => (props.pointer ? 'pointer' : 'default')};
   object-fit: cover;
 `;
