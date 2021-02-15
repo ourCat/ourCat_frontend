45c8f1f20e236ab26a44ad46735dcf9c3f41715a
Denmark-banana <neat507kr@gmail.com>
2021-02-15 22:08:19 +0900

    refactor(components/organisms) 헤더 MAxWidth 추가

--- src/components/organisms/Header/style.ts
+++ src/components/organisms/Header/style.ts
@@ -7,15 +7,21 @@ export const Wrap = styled.header<HeaderProps>`
   height: ${props => props.theme.layoutSizes.HEADER};
   top: 0px;
   left: 0px;
-  box-shadow: 0 2px 6px 2px rgba(0, 0, 0, 0.06);
-  background-color: ${props => props.theme.colors.WHITE};
-  z-index: 5;
+  background-color: ${props => props.theme.colors.GRAY[100]};
 `;
 
-export const Content = styled.header<HeaderProps>`
-  display: flex;
+export const Container = styled.div`
+  max-width: 440px;
   height: 100%;
+  margin: 0 auto;
+  background-color: ${props => props.theme.colors.WHITE};
+`;
+
+export const Content = styled.div`
+  height: 100%;
+  padding: 0 10px;
+  display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 0 10px;
+  box-shadow: 0 2px 6px 2px rgba(0, 0, 0, 0.06);
 `;
