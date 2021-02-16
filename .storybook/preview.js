be0180332e9a338dddbeefb2c8a1093f98c63f56
Denmark-banana <neat507kr@gmail.com>
2021-02-16 23:24:53 +0900

    chore : setting tsconfig-paths-webpack, ThemeProvider with storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,15 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import Themes from 'styles/Theme';
+
+export const decorators = [
+  Story => (
+    <ThemeProvider theme={Themes}>
+      <Story />
+    </ThemeProvider>
+  ),
+];
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+  actions: { argTypesRegex: '^on[A-Z].*' },
+};
