45e71ef7b7dc1f4fbf725e294459fb171d273bfa
Bom Jang <dongwon1826@gmail.com>
2021-02-17 21:31:18 +0900

    refactor(components/organisms) : slider layout 수정

--- src/components/organisms/RecommendList/index.tsx
+++ src/components/organisms/RecommendList/index.tsx
@@ -1,6 +1,5 @@
 import { IComponent } from 'common';
 import { Recommend } from 'components';
-import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import * as S from './style';
@@ -15,54 +14,36 @@ const sampleData = {
     { id: 6, img: '/images/img.png', label: 'cat6', url: '/cat6' },
     { id: 7, img: '/images/img.png', label: 'cat7', url: '/cat7' },
     { id: 8, img: '/images/img.png', label: 'cat8', url: '/cat8' },
-    { id: 9, img: '/images/img.png', label: 'cat8', url: '/cat8' },
-    { id: 10, img: '/images/img.png', label: 'cat8', url: '/cat8' },
+    { id: 9, img: '/images/img.png', label: 'cat9', url: '/cat9' },
+    { id: 10, img: '/images/img.png', label: 'cat10', url: '/cat10' },
   ],
 };
+type dataType = {
+  id: number;
+  img: string;
+  label: string;
+  url: string;
+};
 
-export interface IRecommendListProps extends IComponent {}
+export interface IRecommendListProps extends IComponent {
+  // 서버랑 연결하면 ?를 지울예정입니다.
+  Datas?: dataType[];
+}
 
-export const RecommendList: React.FC<IRecommendListProps> = ({ ...props }) => {
+export const RecommendList: React.FC<IRecommendListProps> = ({ Datas, ...props }) => {
   const settings = {
     infinite: false,
     speed: 500,
     slidesToShow: 5,
     slidesToScroll: 5,
-    initialSlide: 0,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
   };
   return (
     <S.Wrap {...props}>
-      <Slider {...settings}>
+      <S.ReactSlider {...settings}>
         {sampleData.data.map(cat => {
-          return <Recommend img={cat.img} labelText={cat.label} key={cat.id} width={'2rem'} />;
+          return <Recommend img={cat.img} labelText={cat.label} key={cat.id} />;
         })}
-      </Slider>
+      </S.ReactSlider>
     </S.Wrap>
   );
 };
--- src/components/organisms/RecommendList/style.ts
+++ src/components/organisms/RecommendList/style.ts
@@ -1,6 +1,9 @@
 import { ComponentMixin, IComponent } from 'common';
+import Slider from 'react-slick';
 import styled from 'styled-components';
 
 export const Wrap = styled.div<IComponent>`
   ${ComponentMixin};
 `;
+
+export const ReactSlider = styled(Slider)``;
