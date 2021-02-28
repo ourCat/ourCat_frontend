5ceb01a7ac53396526930109a030b7e824dd1308
Denmark-banana <neat507kr@gmail.com>
2021-02-28 19:43:13 +0900

    chore : add img file, add common(imgUrl, pageUrl) resource

---
+++ src/common/paths/imgUrl.ts
@@ -0,0 +1,12 @@
+export const ICON = {
+  /**
+   * Main...
+   */
+  LOGO: '/images/ourCatLogo.png',
+  AVATAR: '/images/img.png',
+  ADDITIONAL: '/images/additional.PNG',
+  HEART: '/images/heart.PNG',
+  TALK: '/images/talk.PNG',
+  USER: '/images/user.PNG',
+  STAR: '/images/star.PNG',
+};
---
+++ src/common/paths/pageUrl.ts
@@ -0,0 +1,8 @@
+export const PAGE = {
+  HOME: '/',
+  CLASS: '/insert',
+  STORE: '/search',
+  MAGAZINE: '/alarm',
+  ME: '/me',
+  DETAIL: (id: number) => `/cat/${id}`,
+};
