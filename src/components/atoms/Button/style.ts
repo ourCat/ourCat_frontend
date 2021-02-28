605919b8e827cd50fc48435b9f924ccd27a10488
spring1826 <dongwon1826@gmail.com>
2021-02-28 20:53:41 +0900

    refactor(components/atoms) : edit button disable (#52)

--- src/components/atoms/Button/style.ts
+++ src/components/atoms/Button/style.ts
@@ -4,33 +4,42 @@ import { ButtonProps } from './index';
 
 export const Wrap = styled.button<ButtonProps>`
   ${ComponentMixin};
-  cursor: pointer;
   ${props => {
-    if (props.filled) {
-      return css`
-        color: ${props => props.theme.colors.WHITE};
-        background-color: ${props => props.theme.colors.MAIN};
-        border: ${props => props.theme.colors.WHITE};
-      `;
-    }
-    if (!props.filled) {
-      return css`
-        color: ${props => props.theme.colors.MAIN};
-        background-color: ${props => props.theme.colors.WHITE};
-        border: ${props => props.theme.colors.MAIN};
-      `;
-    }
     if (props.disabled) {
       return css`
-        color: ${props => props.theme.colors.BLACK};
-        background-color: ${props => props.theme.colors.GRAY[400]};
-        border: ${props => props.theme.colors.GRAY[400]};
+        &:disabled {
+          color: ${props => props.theme.colors.BLACK};
+          background-color: ${props => props.theme.colors.GRAY[400]};
+          border: ${props => props.theme.colors.GRAY[400]};
+        }
       `;
+    } else {
+      if (props.filled) {
+        return css`
+          color: ${props => props.theme.colors.WHITE};
+          background-color: ${props => props.theme.colors.MAIN};
+          border: ${props => props.theme.colors.WHITE};
+          cursor: pointer;
+          &:hover {
+            background-color: ${props => props.theme.colors.DARK};
+          }
+        `;
+      }
+      if (!props.filled) {
+        return css`
+          color: ${props => props.theme.colors.MAIN};
+          background-color: ${props => props.theme.colors.WHITE};
+          border: ${props => props.theme.colors.MAIN};
+          cursor: pointer;
+          &:hover {
+            background-color: ${props => props.theme.colors.MAIN};
+            color: ${props => props.theme.colors.WHITE};
+          }
+        `;
+      }
     }
   }}
-  &:hover {
-    background-color: ${props => props.theme.colors.DARK};
-  }
+
   &:focus {
     outline: none;
   }
