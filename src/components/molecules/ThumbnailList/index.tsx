1a02d326447f55065b9d943e14200ca7c06c6196
Bom Jang <dongwon1826@gmail.com>
2021-03-03 23:33:08 +0900

    feat(components/molcules) : ThumbnailList layout적용

--- src/components/molecules/ThumbnailList/index.tsx
+++ src/components/molecules/ThumbnailList/index.tsx
@@ -1,5 +1,4 @@
 import { Image } from 'components';
-import { ThumbnailListData } from 'common';
 import * as S from './style';
 
 export interface IThumbnailListProps {
@@ -7,13 +6,16 @@ export interface IThumbnailListProps {
 }
 
 export const ThumbnailList: React.FC<IThumbnailListProps> = ({ data }) => {
-  // 이미지 컴포를 이용해서 렌더링
   // TYPE이 이미지인지 동영상인지에 따라 재생버튼 아이콘을 배치하거나 없앤다
-  // 한줄에 3개의 이미지 렌더링하도록 레이아웃 구현
+  // page 구현
   return (
     <S.Wrap>
-      {ThumbnailListData.data.map(imgData => {
-        return <S.ListContainer key={imgData.id}></S.ListContainer>;
+      {data.data.map((imgData: any) => {
+        return (
+          <S.ListContainer key={imgData.id}>
+            <Image src={imgData.imgUrl} width={'100%'} pointer />
+          </S.ListContainer>
+        );
       })}
     </S.Wrap>
   );
--- src/components/molecules/ThumbnailList/style.ts
+++ src/components/molecules/ThumbnailList/style.ts
@@ -1,5 +1,11 @@
 import styled from 'styled-components';
 
-export const Wrap = styled.div``;
+export const Wrap = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 0.2rem;
+  padding: 1rem 0;
+  border-top: 1px solid ${props => props.theme.colors.GRAY[500]};
+`;
 
 export const ListContainer = styled.div``;
