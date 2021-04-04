2da08d0987095cea4763cda2808e150c62d2876c
Denmark-banana <neat507kr@gmail.com>
2021-04-04 16:02:54 +0900

    refactor(templates) - 전체 구조에 templates 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,7 @@
-import { RecommendList, Profile, Card, ThumbnailList } from 'components';
-import { sample, ThumbnailListData } from 'common';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { login } from 'actions';
-import styled from 'styled-components';
-
-export const Wrap = styled.div`
-  min-height: 100vh;
-`;
+import { HomeTemplate } from 'templates';
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -15,29 +9,7 @@ const Home = () => {
     dispatch(login());
   }, []);
 
-  return (
-    <Wrap>
-      <RecommendList />
-      <Profile isCat={false} data={sample} />
-      <Card
-        title={'뚱땅이'}
-        date={new Date()}
-        content={'나는야 김뚱땅. 오늘도 내 짝을 찾는다. \n 대체 어디에 있을까?'}
-        heartCount={230}
-        commentCount={104}
-        bookmarkCount={80}
-      />
-      <Card
-        title={'둥구'}
-        date={new Date()}
-        content={'거기 츄르 있는 사람?\n 츄르 한대만 물자...\n 츄르 한 대 정도는 괜찮잖아?'}
-        heartCount={614}
-        commentCount={311}
-        bookmarkCount={110}
-      />
-      <ThumbnailList data={ThumbnailListData} />
-    </Wrap>
-  );
+  return <HomeTemplate />;
 };
 
 export default Home;
