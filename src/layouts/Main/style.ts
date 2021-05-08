53612c40cb13292fd510dbda2b0e2f699d3c99aa
Bom Jang <dongwon1826@gmail.com>
2021-05-08 21:28:21 +0900

    refactor(layout/main) : main layout 수정

--- src/layouts/Main/style.ts
+++ src/layouts/Main/style.ts
@@ -1,14 +1,17 @@
 import styled from 'styled-components';
 import { MainLayoutProps } from './index';
 
-export const WrapMainLayout = styled.div<MainLayoutProps>`
-  position: relative;
-  padding-top: ${props => props.theme.layoutSizes.HEADER};
-  padding-bottom: ${props => props.theme.layoutSizes.BOTTOM_NAVIGATION};
-  background-color: ${props => props.theme.colors.GRAY[100]};
+export const MainBackgroundImage = styled.div`
+  border: 0;
+  padding: 0;
+  width: 100%;
+  min-height: 100%;
+  background: url('/images/backgroundImage.svg') no-repeat center center fixed;
+  background-position: center;
+  background-size: cover;
 `;
 
-export const Container = styled.div`
+export const WrapMainLayout = styled.div<MainLayoutProps>`
   max-width: 440px;
   margin: 0 auto;
   background-color: ${props => props.theme.colors.WHITE};
