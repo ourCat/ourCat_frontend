924a259feb12e23a738e3c78beba245295eb977e
Denmark-Banana <neat507kr@gmail.com>
2021-07-11 14:31:40 +0900

    refactor - input style, kakakoMap style (#80)

--- src/templates/search/kakaoMap/style.ts
+++ src/templates/search/kakaoMap/style.ts
@@ -7,7 +7,7 @@ export const Wrap = styled.div`
 export const Map = styled.div<{ height?: string; isError?: string }>`
   display: ${props => props.isError && 'none'};
   width: 100%;
-  height: ${props => (props.height ? props.height : '51rem')};
+  height: ${props => (props.height ? props.height : '48rem')};
   border-radius: 0.3rem;
 `;
 
