09e2a00c855c9854a1cf977069026f22c3b1f3e2
spring1826 <dongwon1826@gmail.com>
2021-07-11 18:17:48 +0900

    Feat/add modal component (#81)
    * feat: modal component
    
    * feat : add login modal
    
    * refactor : refactor props method naming

---
+++ src/components/organisms/Modal/LoginModal/style.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const LoginModalWarp = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 70vw;
+  height: 70vh;
+  border-radius: 10px;
+  background-color: white;
+  z-index: 11;
+`;
--- src/templates/myPage/style.ts
+++ src/templates/myPage/style.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const Wrap = styled.div`
+export const MyPageWrap = styled.div`
   width: 100%;
   min-height: 100vh;
 `;
