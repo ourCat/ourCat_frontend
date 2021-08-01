bcbda89e983b536aa18e84557b22042c727bdfa7
Denmark-banana <neat507kr@gmail.com>
2021-08-01 18:51:46 +0900

    feat - add social naverLogin

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   env: {
     KAKAO_JAVASCRIPT_KEY: process.env.KAKAO_JAVASCRIPT_KEY,
+    NAVER_CLIENT_ID: process.env.NAVER_CLIENT_ID,
   },
 };
--- src/templates/index.ts
+++ src/templates/index.ts
@@ -3,3 +3,4 @@ export * from './catDetail';
 export * from './search';
 export * from './withdraw';
 export * from './myPage';
+export * from './snsLogin';
