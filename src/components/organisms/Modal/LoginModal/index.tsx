8caa6d69d0d945a86a274361f24cc95261311396
Bom Jang <dongwon1826@gmail.com>
2021-08-01 18:25:04 +0900

    feat : add Login Modal page

--- src/components/organisms/Modal/LoginModal/index.tsx
+++ src/components/organisms/Modal/LoginModal/index.tsx
@@ -1,6 +1,8 @@
-import * as S from './style';
+import { useForm } from 'react-hook-form';
 import { ModalPortal, Image, Label } from 'components';
 import { ICON } from 'common';
+import * as S from './style';
+import { Button, Input } from 'components/atoms';
 
 export interface ILoginModalProps {
   isLogin: boolean;
@@ -8,11 +10,136 @@ export interface ILoginModalProps {
 }
 
 export const LoginModal: React.FC<ILoginModalProps> = props => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data: any) => console.log(data);
+
   return (
     <ModalPortal handleCloseModal={props.handleCloseModal}>
       <S.LoginModalWarp>
-        <Image src={ICON.LOGO} width="60%" padding="30px 10px" />
-        <Label>로그인 후 이용해주세요</Label>
+        <S.LoginLogoContainer>
+          <Image src={ICON.LOGO} width="60%" padding="10px" />
+          <Label color={'gray'}>로그인 후 이용해주세요</Label>
+        </S.LoginLogoContainer>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <S.LoginLabelContainer>
+            <label htmlFor="name" className="flex_label">
+              이름
+            </label>
+            <Input
+              id="name"
+              type="text"
+              placeholder="이름을 입력해주세요"
+              className="flex_input"
+              {...register('name', { required: true })}
+            />
+          </S.LoginLabelContainer>
+          <S.LoginLabelContainer>
+            <label htmlFor="nick_name" className="flex_label">
+              닉네임
+            </label>
+            <Input
+              id="nick_name"
+              type="text"
+              placeholder="닉네임을 입력해주세요"
+              className="flex_input"
+              {...register('nick_name', { required: true })}
+            />
+          </S.LoginLabelContainer>
+          <S.LoginLabelContainer>
+            <label className="flex_label">성별</label>
+            <select {...register('gender', { required: true })} className="flex_input">
+              <option value="female">female</option>
+              <option value="male">male</option>
+              <option value="other">other</option>
+            </select>
+          </S.LoginLabelContainer>
+          <S.LoginLabelContainer>
+            <label htmlFor="phone_number" className="flex_label">
+              전화번호
+            </label>
+            <Input
+              id="phone_number"
+              type="tel"
+              placeholder="전화번호를 입력해주세요"
+              className="flex_input"
+            />
+          </S.LoginLabelContainer>
+          <S.LoginLabelContainer>
+            <label htmlFor="email" className="flex_label">
+              이메일
+            </label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="이메일을 입력해주세요"
+              className="flex_input"
+              {...register('email', { required: true })}
+            />
+          </S.LoginLabelContainer>
+          <S.LoginLabelContainer>
+            <label htmlFor="password" className="flex_label">
+              비밀번호
+            </label>
+            <Input
+              id="password"
+              type="password"
+              placeholder="비밀번호를 입력해주세요"
+              className="flex_input"
+              {...register('password', { required: true })}
+            />
+          </S.LoginLabelContainer>
+          <S.LoginLabelContainer>
+            <label htmlFor="password_check" className="flex_label">
+              비밀번호 확인
+            </label>
+            <Input
+              id="password_check"
+              type="password"
+              placeholder="비밀번호를 입력해주세요"
+              className="flex_input"
+              {...register('password_check', { required: true })}
+            />
+          </S.LoginLabelContainer>
+          {errors.name &&
+            errors.nick_name &&
+            errors.gender &&
+            errors.email &&
+            errors.password &&
+            errors.password_check && <span>This field is required</span>}
+          <Button
+            onClick={() => {}}
+            filled
+            label="Login"
+            padding="8px 12px"
+            borderRadius="4px"
+            width="100%"
+          />
+        </form>
+        <S.LoginSpaceBar />
+        <S.LoginSocialContainer>
+          <Button
+            onClick={() => {}}
+            filled
+            label="kakao"
+            padding="8px 0"
+            borderRadius="4px"
+            width="100%"
+            margin="0 8px 0 0"
+          />
+          <Button
+            onClick={() => {}}
+            filled
+            label="google"
+            padding="8px 0"
+            borderRadius="4px"
+            width="100%"
+          />
+        </S.LoginSocialContainer>
       </S.LoginModalWarp>
     </ModalPortal>
   );
--- src/components/organisms/Modal/LoginModal/style.ts
+++ src/components/organisms/Modal/LoginModal/style.ts
@@ -5,9 +5,42 @@ export const LoginModalWarp = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  width: 70vw;
+  width: 80vw;
   height: 70vh;
   border-radius: 10px;
   background-color: white;
   z-index: 11;
+  padding: 16px;
+`;
+
+export const LoginLogoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 2rem;
+`;
+
+export const LoginLabelContainer = styled.div`
+  display: flex;
+  padding: 4px 0;
+  .flex_label {
+    flex: 1;
+  }
+  .flex_input {
+    flex: 2;
+  }
+`;
+
+export const LoginSpaceBar = styled.div`
+  width: 100%;
+  height: 1px;
+  background-color: gray;
+  margin: 8px 0;
+`;
+
+export const LoginSocialContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
 `;
