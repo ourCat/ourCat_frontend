788a5ecf6ea3d47acb6ffb047d5ba6809977dd3a
Daehyun <neat507kr@gmail.com>
2021-08-29 16:52:31 +0900

    Revert "refactor - input style, kakakoMap style (#80)"
    This reverts commit 924a259feb12e23a738e3c78beba245295eb977e.

--- src/components/atoms/Input/style.ts
+++ src/components/atoms/Input/style.ts
@@ -4,18 +4,4 @@ import { ComponentMixin } from 'common';
 
 export const WrapInput = styled.input<IInputProps>`
   ${ComponentMixin};
-
-  display: ${props => (props.display ? props.display : 'block')};
-  padding: ${props => props.padding && props.padding};
-  width: 100%;
-  height: ${props => props.height && props.height};
-  font: ${props => props.theme.fonts[14]};
-  letter-spacing: -1px;
-  border: ${props => props.border && props.border};
-
-  &::placeholder {
-    font: inherit;
-    letter-spacing: inherit;
-    color: ${props => props.theme.colors.GRAY4};
-  }
 `;
--- src/templates/search/style.ts
+++ src/templates/search/style.ts
@@ -3,7 +3,3 @@ import styled from 'styled-components';
 export const Wrap = styled.div`
   min-height: 91vh;
 `;
-
-export const InputBox = styled.div`
-  padding: 10px;
-`;
