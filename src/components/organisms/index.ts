9a99c7c4d468ad9163d26bfbae5bb5b4afe84d0e
Daehyun <neat507kr@gmail.com>
2021-08-29 16:52:31 +0900

    Revert "feat(components/arganisms) : add profile component (#58)"
    This reverts commit baf98735691564cee1ce2568181ceca954c9c388.

--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -1,4 +1,3 @@
 export * from './Header';
 export * from './RecommendList';
 export * from './BottomNavigation';
-export * from './Profile';
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
-
-import { Label, CheckBox, RecommendList, Profile, Card } from 'components';
+import { Card, RecommendList } from 'components';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { login } from 'actions';
@@ -9,40 +8,14 @@ export const Wrap = styled.div`
 `;
 
 const Home = () => {
-  const sample = {
-    name: '뚱땅이',
-    profileSrc: 'images/img.png',
-    profileContent: [
-      { id: 1, name: '팔로워', count: '1.7k' },
-      { id: 2, name: '게시물', count: '182' },
-      { id: 3, name: '게시자', count: '12' },
-    ],
-    tag: [
-      { id: 1, icon: 'icon', text: '강남구' },
-      { id: 1, icon: 'icon', text: '먼치킨' },
-      { id: 1, icon: 'icon', text: '3살(추정)' },
-    ],
-    description: '츄르를 많이 좋아해요',
-    createAt: '2021-02-02',
-    updateAt: '2021-02-23',
-  };
-
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(login());
   }, []);
 
-
   return (
     <Wrap>
       <RecommendList />
-      <CheckBox hasLabel isChecked>
-        <Label font={14} weight="bold" margin={'0 0 0 10px'} height="1080px">
-          하이
-        </Label>
-      </CheckBox>
-      <Profile isCat={false} data={sample} />
-    </>
       <Card
         title={'뚱땅이'}
         date={new Date()}
