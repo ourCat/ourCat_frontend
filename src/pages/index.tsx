da6030c84b47c44340fe9eba66c39954e68f94cb
Daehyun <neat507kr@gmail.com>
2021-08-29 16:52:31 +0900

    Revert "refactor(templates) - 전체 구조에 templates 추가 (#66)"
    This reverts commit de1877936e103ddb2bcba34b61009634cf162816.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
+import { RecommendList, Profile, Card, ThumbnailList } from 'components';
+import { sample, ThumbnailListData } from 'common';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { login } from 'actions';
-import { HomeTemplate } from 'templates';
+import styled from 'styled-components';
+
+export const Wrap = styled.div`
+  min-height: 100vh;
+`;
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -9,7 +15,29 @@ const Home = () => {
     dispatch(login());
   }, []);
 
-  return <HomeTemplate />;
+  return (
+    <Wrap>
+      <RecommendList />
+      <Profile isCat={false} data={sample} />
+      <Card
+        title={'뚱땅이'}
+        date={new Date()}
+        content={'나는야 김뚱땅. 오늘도 내 짝을 찾는다. \n 대체 어디에 있을까?'}
+        heartCount={230}
+        commentCount={104}
+        bookmarkCount={80}
+      />
+      <Card
+        title={'둥구'}
+        date={new Date()}
+        content={'거기 츄르 있는 사람?\n 츄르 한대만 물자...\n 츄르 한 대 정도는 괜찮잖아?'}
+        heartCount={614}
+        commentCount={311}
+        bookmarkCount={110}
+      />
+      <ThumbnailList data={ThumbnailListData} />
+    </Wrap>
+  );
 };
 
 export default Home;
