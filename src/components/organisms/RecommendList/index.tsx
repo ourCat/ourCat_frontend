ff6865330ad348b1ad450fe59ab7fdefc0c2306f
Daehyun <neat507kr@gmail.com>
2021-08-29 16:52:31 +0900

    Revert "refactor(components/molecules) : remove arrows (#51)"
    This reverts commit 8dc149d144e7b1486f83380132cca796ac6fd86a.

--- src/components/organisms/RecommendList/index.tsx
+++ src/components/organisms/RecommendList/index.tsx
@@ -38,9 +38,9 @@ export const RecommendList: React.FC<IRecommendListProps> = ({ Datas, ...props }
     speed: 500,
     slidesToShow: 5,
     slidesToScroll: 5,
-    arrows: false,
+    isArrow: false,
   };
-  const onImgClick = (url: string) => {
+  const handleImgUrl = (url: string) => {
     router.push(url);
   };
   return (
@@ -51,7 +51,7 @@ export const RecommendList: React.FC<IRecommendListProps> = ({ Datas, ...props }
             <Recommend
               img={cat.img}
               labelText={cat.label}
-              onClick={() => onImgClick(cat.url)}
+              onClick={() => handleImgUrl(cat.url)}
               key={cat.id}
             />
           );
