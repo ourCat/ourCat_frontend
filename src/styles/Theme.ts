5449e793c5c6668a31ad2ee7f6c1ad958893128f
Daehyun <neat507kr@gmail.com>
2021-08-29 16:52:31 +0900

    Revert "style: add GlobalStyles & Theme"
    This reverts commit ab560e71c593564b64522970b5d60c0d25f82c84.

--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -26,7 +26,6 @@ const colors: { [key: string]: string | object } = {
  * Regular 400
  * Bold 700
  */
-
 const fonts: { [key: number]: string } = {
   48: '48px/58px SpoqaHanSans',
   36: '36px/48px SpoqaHanSans',
