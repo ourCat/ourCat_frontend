3ec8c557fe02299c8cce89b8f14aab88d9204e31
Daehyun <neat507kr@gmail.com>
2021-10-30 18:05:56 +0900

    chore: 소셜 로그인 (네이버, 카카오) 연결 (#97)
    * chore: 소셜 로그인 (네이버, 카카오) 연결
    1. 콜백 페이지에서 소셜로그인 API 호출
    2. 환경변수 development, production 용 도메인 설정
    
    * fix: remove console.log

--- src/pages/callback/kakao.tsx
+++ src/pages/callback/kakao.tsx
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router';
 import { PAGE } from 'common';
 import axios from 'axios';
 import { Label } from 'components';
+import { USER_API } from 'api/setting';
 
 declare global {
   interface Window {
@@ -17,13 +18,12 @@ const Kakao = () => {
   const error = router.query.error;
 
   const getKakaoToken = async (code: string | string[] | undefined) => {
-    console.log('clientId: ', `${process.env.KAKAO_CLIENT_ID}`);
     if (!code) return;
     //2. 인증코드를 통해 사용자 토큰 요청
     const result: any = await axios.get('https://kauth.kakao.com/oauth/token', {
       params: {
         grant_type: 'authorization_code',
-        client_id: `${process.env.KAKAO_CLIENT_ID}`, //KAKAO API KEY
+        client_id: `${process.env.KAKAO_JAVASCRIPT_KEY}`, //KAKAO API KEY
         redirect_uri: PAGE.KAKAO_CALLBACK,
         code,
       },
@@ -31,23 +31,17 @@ const Kakao = () => {
 
     const access_token = result.data.access_token;
     if (access_token) {
-      console.log(access_token);
-      if (process.browser) {
-        const { Kakao } = window;
-        if (!Kakao.isInitialized()) Kakao.init(`${process.env.KAKAO_JAVASCRIPT_KEY}`); //KAKAO JAVASCRIPT KEY
+      socialLogin(access_token);
+    }
+  };
 
-        //3. 사용자 토큰 할당 후 사용자 정보 요청 함수 호출
-        Kakao.Auth.setAccessToken(access_token);
-        Kakao.API.request({
-          url: '/v2/user/me',
-          success: function (response: any) {
-            console.log(response);
-          },
-          fail: function (error: any) {
-            console.log(error);
-          },
-        });
-      }
+  const socialLogin = async (token: string) => {
+    const result = await USER_API.post('/user/social/kakao', {
+      token,
+    });
+    if (result.status === 200) {
+      const { userToken } = result.data;
+      alert(`카카오 소셜 로그인 완료\ntoken: ${userToken}`);
     }
   };
 
